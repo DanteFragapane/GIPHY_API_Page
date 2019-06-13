e8c5169d797f21c17fb043e8993f8e0d71b0453b
Dante Fragapane <dante.fragapane@gmail.com>
2019-06-12 20:59:44 -0400

    Finished the initializeButtons function, added comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,41 @@
 let topics = ['aviation', 'airplane', 'plane', 'boeing', 'airbus', 'b737', 'a380', 'takeoff', 'rocket', 'spacex', 'tesla']
 
+// ======================
+// Functions
+// ======================
+// Initializes the topicArray as buttons on the page
 const initializeButtons = function initializeButtons(topicArray) {
   $('#topics').html('')
-  topicArray.forEach((topic) => {
-    console.log(topic)
+  let buttons = $('<div>', {
+    id: 'buttonList'
   })
+  topicArray.forEach((topic) => {
+    let button = $('<button>', {
+      class: 'topicButton',
+      id: topic,
+      text: topic
+    })
+    buttons.append(button)
+  })
+  console.log(buttons)
+  $('#topics').append(buttons)
 }
 
+// Adds the given topic to the given topics array
 const addTopic = function addTopic(topicName, topics) {
-  if(!topics.includes(topicName)) {
+  if (!topics.includes(topicName)) {
     topics.push(topicName)
   } else return false
 }
+// ======================
+
+// ======================
+// Logic
+// ======================
 
 initializeButtons(topics)
 
+// On submit of the form
 $('form').on('submit', (event) => {
   event.preventDefault()
 
